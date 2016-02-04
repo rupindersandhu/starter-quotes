9db38be60d74cbccb446cf178d07ece92e169742
Renan Aguiar <renan@rca.pro.br>
2016-02-04 15:11:24 -0800

    fixed link 6 added image to data folder

--- application/config/routes.php
+++ application/config/routes.php
@@ -42,6 +42,14 @@ $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 $route['sleep'] = 'first/zzz';
 $route['show/(:num)'] = 'first/gimme/$1';
+$route['dunno'] = function() {
+	$source = './data/free.jpg'; // an image you provide
+	// set the mime type for that image
+	header("Content-type: image/jpeg"); 
+	header('Content-Disposition: inline');
+	readfile($source); // dish it
+	die(); // and we don't have to go any further
+};
 
 
 /* End of file routes.php */
