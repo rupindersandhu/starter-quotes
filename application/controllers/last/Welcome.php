b45dd374a770fc43157d5dafed0d75ef8c12c3a8
mebighobo <john_chang69@hotmail.com>
2016-02-04 13:05:56 -0800

    Fix the second link.

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Welcome extends Application {
+	function __construct()
+	{
+		parent::__construct();
+	}
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
+	function index()
+	{
+		$this->data['pagebody'] = 'justone';
+		$record = $this->quotes->last();
+		$this->data = array_merge($this->data, $record);
+		$this->render();
+	}
+}
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
